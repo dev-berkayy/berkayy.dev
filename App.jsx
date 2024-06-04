81f81f998adc24191fcfae6cddf9facb22d2d1d7
Berkay-yıldırım <152439115+dev-berkayy@users.noreply.github.com>
2024-06-04 14:31:49 +0300

    UPGRADE

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Section1 from './components/section1'
+import Section2 from './components/section2'
+
+import "./Global.css"
+function App() {
+  return (
+    <div className='app'>
+
+      <Section1 />
+      <Section2 />
+
+    </div>
+  )
+}
+
+export default App
